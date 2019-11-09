1c1a7f798a54b887000f6fc130c813e1fa88f90e
cas8903 <cas8903@rit.edu>
2019-11-09 15:20:45 -0500

    beginner reddit reading

--- askReddit.py
+++ askReddit.py
@@ -1,18 +1,35 @@
-import praw
+""""""""""Chris Sequeira in conjunction with Connor Jackson
+HackPrinceton2019"""""""""
+
 #go to preferences, project interpreter and search 'praw' to install to pycharm
+import praw
+#use csv files for relational sumbissions->comments databases
+import csv
 
 
-#TODO
 #creates an instance of reddit
-reddit=praw.reddit(args)
+reddit=praw.reddit(user_agent='Sentiment analysis by Connor Jackson, Chris Sequeira')
 #enable read only mode
 reddit.read_only=True
-
-#TODO define reddit instance
+#define reddit instance
 askReddit = reddit.subreddit('AskReddit')
-#TODO read all submissions into a database
-#TODO read all comments into a relational database
-#use MONGO DB?
+
+"""make a dictionary
+keys = submissions
+values = comments"""
+posts = {}
+
+#TODO narrow by time
+#writes all submissions to a dictionary
+for submission in askReddit.submissions.topAllTime():
+    posts[submission]
+    #writes each cubmission's comments
+    #TODO fix
+    for commentsCSV in submission:
+        #add comments to new list every time
+        #connect list to key
+        posts[submission].add(comment)
+
 
 
 

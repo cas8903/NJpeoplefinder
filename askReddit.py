4a9c798467e70963616cb253992f1b9a695141e4
cas8903 <cas8903@rit.edu>
2019-11-09 20:27:54 -0500

    changed to postID

--- askReddit.py
+++ askReddit.py
@@ -1,7 +1,7 @@
-""""""""""Chris Sequeira in conjunction with Connor Jackson
-HackPrinceton2019"""""""""
+"""Chris Sequeira
+    Connor Jackson
+    HackPrinceton 2019"""
 
-#go to preferences, project interpreter and search 'praw' to install to pycharm
 import praw
 import gcp
 
@@ -10,39 +10,39 @@ import gcp
 reddit=praw.Reddit(user_agent='Sentiment analysis by Connor Jackson, Chris Sequeira', client_id='OPbpwRMkKTA1VA', client_secret="0CjJ_J3DrkyPUk3O2V2fE1dfJII", )
 #enable read only mode
 reddit.read_only=True
-#define reddit instance
-askReddit = reddit.subreddit('AskReddit')
 
-#may be unnecessary
-#__init__(site_name=None, requestor_class=None, requestor_kwargs=None, **config_settings)
+"""make a list format for values, submissions and comments"""
+comments=[]
 
-"""make a dictionary
-with a list format
-for values
-keys = submissions
-values = comments"""
-posts = {}
-key=""
-posts.setdefault(key, [])
+"""builds post from reddit link"""
+def parseID(postid):
+    submission = reddit.submission(id=postid)
+    print("Title: ",submission.title)
+    return submission
 
-#TODO narrow by time
-#writes all submissions to a dictionary
-for post in askReddit.submission.topAllTime():
-    posts[post]
-    #writes each cubmission's comments
-    for top_level_comment in post.comments:
+"""Writes comment body and post title to separate lists"""
+def buildContent(submission):
+    #strips noncomment interface components
+    submission.comments.replace_more(limit=0)
+    #writes each submission's comments
+    for top_level_comment in submission.comments:
         #appends comments to the list of values
-        posts[post].append(top_level_comment.body)
+        comments.append(top_level_comment.body)
+    return comments
 
-def gather_sentiment(posts):
-    for i in posts.keys():
-        for j in posts[i]:
-            gcp.gather_sentiment(j)
+"""Passes content to google for analysis"""
+def gather_sentiment(comments):
+    for comment in comments:
+        gcp.gather_sentiment(comment)
+
 
 def main():
-    for content in posts.items():
-        print(content)
+    postid = input("Enter a reddit postID to evaluate sentiment: ")
+    comments=buildContent(parseID(postid))
+    gather_sentiment(comments)
 
     return 0
+
+main()
 
 
